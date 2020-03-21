9e5a4d13a8ebee109067ded84e2d3a586c12391f
Thomas Waldecker <thomas.waldecker@gmail.com>
2020-03-21 14:24:41 +0100

    Frontend: double triangle

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,20 +5,19 @@ function height(length: number) {
   return Math.sqrt(length ** 2 - (length / 2) ** 2);
 }
 
-function App() {
+function Triangle() {
   let base = 500;
   let base_height = height(base);
-
 
   let startx = 200;
   let starty = 200;
 
   let lines: any[] = [];
 
-  let countLines = 20;
+  let countLines = 11;
   let small_triangle = 500 / (countLines + 1);
 
-  for(let i = 1; i <= countLines; i++) {
+  for (let i = 1; i <= countLines; i++) {
     lines.push({
       ax: startx + (small_triangle / 2) * i,
       ay: starty + height(small_triangle) * i,
@@ -27,20 +26,35 @@ function App() {
       cx: startx + small_triangle * i,
       cy: starty,
       dx: startx + base - small_triangle * i,
-      dy: starty,
+      dy: starty
     });
   }
 
   return (
-    <svg height="900" width="900">
+    <>
       <path
         d={`M ${startx} ${starty} l ${base} 0 l -${base / 2} ${base_height} z`}
         fill="none"
         stroke="#999"
       />
       {lines.map(x => (
-        <path d={`M ${x.cx} ${x.cy} L ${x.ax} ${x.ay} L ${x.bx} ${x.by} L ${x.dx} ${x.dy}`} fill="none" stroke="#999"/>
+        <path
+          d={`M ${x.cx} ${x.cy} L ${x.ax} ${x.ay} L ${x.bx} ${x.by} L ${x.dx} ${x.dy}`}
+          fill="none"
+          stroke="#999"
+        />
       ))}
+    </>
+  );
+}
+
+function App() {
+  return (
+    <svg height="900" width="900">
+      <g>{Triangle()}</g>
+      <g transform={`rotate(180, ${200 + 500/2}, ${200+height(500)/2}) translate(0, ${height(4*500/12)})`}>
+      {Triangle()}
+      </g>
     </svg>
   );
 }
