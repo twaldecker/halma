acbe3ac82c36d25d44e04451e85038fb790864c5
Thomas Waldecker <thomas.waldecker@gmail.com>
2020-03-21 14:16:26 +0100

    Frontend: draw svg triangle with configurable triangle grid

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+function height(length: number) {
+  return Math.sqrt(length ** 2 - (length / 2) ** 2);
+}
 
 function App() {
+  let base = 500;
+  let base_height = height(base);
+
+
+  let startx = 200;
+  let starty = 200;
+
+  let lines: any[] = [];
+
+  let countLines = 20;
+  let small_triangle = 500 / (countLines + 1);
+
+  for(let i = 1; i <= countLines; i++) {
+    lines.push({
+      ax: startx + (small_triangle / 2) * i,
+      ay: starty + height(small_triangle) * i,
+      bx: startx + base - (small_triangle / 2) * i,
+      by: starty + height(small_triangle) * i,
+      cx: startx + small_triangle * i,
+      cy: starty,
+      dx: startx + base - small_triangle * i,
+      dy: starty,
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <svg height="900" width="900">
+      <path
+        d={`M ${startx} ${starty} l ${base} 0 l -${base / 2} ${base_height} z`}
+        fill="none"
+        stroke="#999"
+      />
+      {lines.map(x => (
+        <path d={`M ${x.cx} ${x.cy} L ${x.ax} ${x.ay} L ${x.bx} ${x.by} L ${x.dx} ${x.dy}`} fill="none" stroke="#999"/>
+      ))}
+    </svg>
   );
 }
 
