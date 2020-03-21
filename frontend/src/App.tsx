f4f12e23b06e18c4b6985e2b091a6c7312161a2a
Thomas Waldecker <thomas.waldecker@gmail.com>
2020-03-21 14:54:06 +0100

    Frontend: responsive sizing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,16 +5,11 @@ function height(length: number) {
   return Math.sqrt(length ** 2 - (length / 2) ** 2);
 }
 
-function Triangle() {
-  let base = 500;
+function Triangle(base: number, startx: number, starty: number, countLines: number) {
   let base_height = height(base);
-
-  let startx = 200;
-  let starty = 200;
 
   let lines: any[] = [];
 
-  let countLines = 11;
   let small_triangle = 500 / (countLines + 1);
 
   for (let i = 1; i <= countLines; i++) {
@@ -36,24 +31,31 @@ function Triangle() {
         d={`M ${startx} ${starty} l ${base} 0 l -${base / 2} ${base_height} z`}
         fill="none"
         stroke="#999"
-      />
+        />
       {lines.map(x => (
         <path
-          d={`M ${x.cx} ${x.cy} L ${x.ax} ${x.ay} L ${x.bx} ${x.by} L ${x.dx} ${x.dy}`}
-          fill="none"
-          stroke="#999"
+        d={`M ${x.cx} ${x.cy} L ${x.ax} ${x.ay} L ${x.bx} ${x.by} L ${x.dx} ${x.dy}`}
+        fill="none"
+        stroke="#999"
         />
-      ))}
+        ))}
     </>
   );
 }
 
 function App() {
+  let base = 500;
+
+  let startx = 10;
+  let starty = 150;
+
+  let countLines = 11;
+
   return (
-    <svg height="900" width="900">
-      <g>{Triangle()}</g>
-      <g transform={`rotate(180, ${200 + 500/2}, ${200+height(500)/2}) translate(0, ${height(4*500/12)})`}>
-      {Triangle()}
+    <svg viewBox="0 0 520 600" style={{maxHeight: '100vh', width: '100%'}}>
+      <g>{Triangle(base, startx, starty, countLines)}</g>
+      <g transform={`rotate(180, ${startx + base/2}, ${starty+height(base)/2}) translate(0, ${height(4*base/(countLines+1))})`}>
+      {Triangle(base, startx, starty, countLines)}
       </g>
     </svg>
   );
