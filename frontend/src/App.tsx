1070d90f4eda9f252de1473425eb1dd75951a705
Thomas Waldecker <thomas.waldecker@gmail.com>
2020-03-21 15:04:04 +0100

    Frontend: Simplify countLines, add Pin

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,14 +5,27 @@ function height(length: number) {
   return Math.sqrt(length ** 2 - (length / 2) ** 2);
 }
 
-function Triangle(base: number, startx: number, starty: number, countLines: number) {
+function Pin(x: number, y: number, color: string) {
+  return (
+    <>
+      <circle r="5" fill={color} cx={x} cy={y}></circle>
+    </>
+  );
+}
+
+function Triangle(
+  base: number,
+  startx: number,
+  starty: number,
+  countLines: number
+) {
   let base_height = height(base);
 
   let lines: any[] = [];
 
-  let small_triangle = 500 / (countLines + 1);
+  let small_triangle = 500 / countLines;
 
-  for (let i = 1; i <= countLines; i++) {
+  for (let i = 1; i < countLines; i++) {
     lines.push({
       ax: startx + (small_triangle / 2) * i,
       ay: starty + height(small_triangle) * i,
@@ -31,14 +44,14 @@ function Triangle(base: number, startx: number, starty: number, countLines: numb
         d={`M ${startx} ${starty} l ${base} 0 l -${base / 2} ${base_height} z`}
         fill="none"
         stroke="#999"
-        />
+      />
       {lines.map(x => (
         <path
-        d={`M ${x.cx} ${x.cy} L ${x.ax} ${x.ay} L ${x.bx} ${x.by} L ${x.dx} ${x.dy}`}
-        fill="none"
-        stroke="#999"
+          d={`M ${x.cx} ${x.cy} L ${x.ax} ${x.ay} L ${x.bx} ${x.by} L ${x.dx} ${x.dy}`}
+          fill="none"
+          stroke="#999"
         />
-        ))}
+      ))}
     </>
   );
 }
@@ -49,14 +62,21 @@ function App() {
   let startx = 10;
   let starty = 150;
 
-  let countLines = 11;
+  let countLines = 12;
 
   return (
-    <svg viewBox="0 0 520 600" style={{maxHeight: '100vh', width: '100%'}}>
+    <svg viewBox="0 0 520 600" style={{ maxHeight: "100vh", width: "100%" }}>
       <g>{Triangle(base, startx, starty, countLines)}</g>
-      <g transform={`rotate(180, ${startx + base/2}, ${starty+height(base)/2}) translate(0, ${height(4*base/(countLines+1))})`}>
-      {Triangle(base, startx, starty, countLines)}
+      <g
+        transform={`rotate(180, ${startx + base / 2}, ${starty +
+          height(base) / 2}) translate(0, ${height(
+          (4 * base) / countLines
+        )})`}
+      >
+        {Triangle(base, startx, starty, countLines)}
       </g>
+      {Pin(10, 150, "#f0f")}
+      {Pin(510, 150, "#f00")}
     </svg>
   );
 }
