9d6c1cfe48efd0fdb838aff8ae63c49e822686e6
Andy Wunderlich <andy.wunderlich@gmail.com>
2020-03-22 17:07:20 +0100

    Added counter for connection & disconnection in backend

--- backend/src/channels.ts
+++ backend/src/channels.ts
@@ -8,12 +8,30 @@ export default function(app: Application) {
     return;
   }
 
+  let channelConnections: any = [];
+
   app.on('connection', (connection: any) => {
     let channel = new URL(connection.headers.referer).pathname
+
+    if (channelConnections[channel] == undefined) {
+      channelConnections[channel] = 0;  
+    }
+
+    channelConnections[channel]++;
+    //console.log("Connections: " + channel + ": " + channelConnections[channel]);
     app.channel(channel).join(connection);
   });
 
+  app.on('disconnect', (connection: any) => {
+    let channel = new URL(connection.headers.referer).pathname
+    channelConnections[channel]--;
+    //console.log("Disconnected from " + channel);
+    //console.log("Connections: " + channel + ": " + channelConnections[channel]);
+  })
+
   app.publish((msg: any) => {
+    //msg.data.channel.connections = channelConnections[msg.data.channel.data.channel];
+    //console.log(msg.data.channel.updated);
     return app.channel(msg.data.channel);
   });
 
