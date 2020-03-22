5ea385ed7adcf4da22f27736a9e68cd5e0996278
Andy Wunderlich <andy.wunderlich@gmail.com>
2020-03-22 17:49:47 +0100

    Added whatsapp share for mobile

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -240,6 +240,7 @@ function App() {
     <div className="game">
     <h1>Halma</h1>
     <div className="spielid">Spiel ID: <a href={window.location.href}>{window.location.pathname.substr(1)}</a></div>
+    <div className="share"><a href={"whatsapp://send?text=Ich+möchte+mit+Dir+Halma+spielen.+Jetzt+hier+klicken:+halma.qurdo.com"} data-action="share/whatsapp/share"><img src="whatsapp.png"></img></a></div>
     <svg viewBox="0 0 540 620" style={{ maxHeight: "calc(100vh - 50px)", width: "100%" }} onClick={unselect}>
       <g>{Triangle(base, startx, starty, countLines)}</g>
       <g
